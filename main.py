1c6ecea277098e51ed2611d014242e18d28bbcba
re-indweller <asf.ayrat@gmail.com>
2024-08-02 20:21:43 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+print("1st program")# 1st program
+print(9**0.5*5)
+
+print("2nd program")# 2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+print("3rd program")# 3rd program
+a = 2*2+2
+b = 2*(2+2)
+print(a == b)
+
+print("4th program")# 4th program
+x = 123.456
+print(int((x*10)%(x//1)))
